7dedf651c980037b95a6f844af7466e169a122f3
Shreepono <167643285+shreepornoCoder@users.noreply.github.com>
2024-12-13 19:37:50 +0600

    file uploaded

---
+++ A_YES_or_YES.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(0, t):
+    condition = 'yes'
+    s = input()
+
+    s = s.lower()
+
+    if s == condition:
+        print("YES")
+
+    else:
+        print("NO")
